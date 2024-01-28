971e4b91a0cd443969f572fb3a38083c287955e9
isso-719 <i@kazukiisogai.net>
2024-01-28 15:29:03 +0900

    fix sitemap

--- scripts/gen_sitemap.mjs
+++ scripts/gen_sitemap.mjs
@@ -10,6 +10,7 @@ const staticPages = [
 ];
 
 let pages = staticPages;
+const date = new Date();
 
 const articlePrefix = "/blog/";
 let allArticles = fs.readdirSync("public/static/articles/");
@@ -23,13 +24,16 @@ for (const tag of Object.keys(tags)) {
   pages.push(tagPrefix + tag);
 }
 
-let sitemap = `<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+let sitemap = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`;
 
 for (const page of pages) {
-  sitemap += `<url><loc>${domain}${page}</loc></url>`;
+  const path = page.split("/").filter((p) => p !== "");
+  const priority = 0.8 ** path.length;
+
+  sitemap += `<url><loc>${domain}${page}</loc><lastmod>${date.toISOString()}</lastmod><priority>${priority.toFixed(2)}</priority></url>`;
 }
 
-sitemap += `</sitemapindex>`;
+sitemap += `</urlset>`;
 
 fs.writeFileSync("public/sitemap.xml", sitemap);
 console.log("Generated public/sitemap.xml");
