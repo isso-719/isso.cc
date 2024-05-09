fff6e186997bd3317bbdee397e679ec826958740
isso-719 <i@kazukiisogai.net>
2024-05-09 18:28:43 +0900

    disable search button

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -18,13 +18,13 @@ const Navigation = () => {
                         <Link href="/tags" className="hidden sm:block">TAGS</Link>
                         <Link href="/about" className="hidden sm:block">ABOUT</Link>
                         {/* TODO: algolia search */}
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                 stroke="currentColor" className="h-6 w-6 text-gray-900 dark:text-gray-100">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                      d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
-                            </svg>
-                        </button>
+                        {/*<button>*/}
+                        {/*    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"*/}
+                        {/*         stroke="currentColor" className="h-6 w-6 text-gray-900 dark:text-gray-100">*/}
+                        {/*        <path strokeLinecap="round" strokeLinejoin="round"*/}
+                        {/*              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>*/}
+                        {/*    </svg>*/}
+                        {/*</button>*/}
                         <SwitchThemeBtn/>
                         <div className="sm:hidden">
                             <button type="button"
